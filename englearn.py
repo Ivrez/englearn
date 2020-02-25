539fcd5441d3026ac436496a43ca3f4e990dbe98
ivrez <reznichivan@gmail.com>
2020-02-25 03:32:55 +0300

    SQLite test code added

---
+++ englearn.py
@@ -0,0 +1,30 @@
+import sqlite3
+import sys
+
+from googletrans import Translator
+
+conn = sqlite3.connect("englearn.db")
+cur = conn.cursor()
+
+sql = '''\
+CREATE TABLE words (
+    word_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    word_en TEXT NOT NULL,
+    word_ru TEXT NOT NULL,
+    word_rating INTEGER NOT NULL,
+    time_added INTEGER NOT NULL
+);
+CREATE TABLE word_groups (
+    word_group_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT NOT NULL
+);
+'''
+
+try:
+    cur.executescript(sql)
+except sqlite3.DatabaseError as err:
+    print("Error: ", err)
+    sys.exit()
+
+print("Successfully executed")
+
